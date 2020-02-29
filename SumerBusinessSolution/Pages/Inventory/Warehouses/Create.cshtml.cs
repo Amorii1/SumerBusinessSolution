3378a967fd0d437227200ce8f952025fba9200bc
Omar Osamah <Amoriimessi@users.noreply.github.com>
2020-02-29 11:46:23 +0300

    Create warehouse correction

--- SumerBusinessSolution/Pages/Inventory/Warehouses/Create.cshtml.cs
+++ SumerBusinessSolution/Pages/Inventory/Warehouses/Create.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,17 +24,26 @@ namespace SumerBusinessSolution.Pages.Inventory.Warehouses
         }
         [BindProperty]
         public Warehouse Warehouse { get; set; }
+        [Required]
         public WhType WhType { get; set; }
         public int TypeId { get; set; }
 
         public async Task<IActionResult> OnGet()
         {
+
+           
+
             WhType = await _db.WhType.FirstOrDefaultAsync(t => t.Id == TypeId);
             return Page();
         }
        
         public async Task<IActionResult> OnPost()
         {
+            var ClaimId = (ClaimsIdentity)User.Identity;
+            var Claim = ClaimId.FindFirst(ClaimTypes.NameIdentifier);
+            string UserId = Claim.Value;
+
+            Warehouse.CreatedById = UserId;
           //  Warehouse.CreatedById = Warehouse.ApplicationUser.Id;
             _db.Warehouse.Add(Warehouse);
             await _db.SaveChangesAsync();
