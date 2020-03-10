3f5285bf6fbf55ca29f0404cb07c6d86688ea336
sumersolutions <sumersolutions@users.noreply.github.com>
2020-03-10 09:53:51 +0300

    fixing redirect to login page bug

--- SumerBusinessSolution/Startup.cs
+++ SumerBusinessSolution/Startup.cs
@@ -80,6 +80,13 @@ namespace SumerBusinessSolution
                     return factory.Create(nameof(CommonResources), assemblyName.Name);
                 };
             });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Identity/Account/Login";
+                options.LogoutPath = "/Identity/Account/Logout";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
