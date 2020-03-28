772c4078acd27da7c6af3913e942b8433ab4fd35
Omar Osamah <Amoriimessi@users.noreply.github.com>
2020-03-28 13:58:28 +0300

    Conflicted Pages solved

--- SumerBusinessSolution/Pages/Inventory/IncomingGoods/Index.cshtml.cs
+++ SumerBusinessSolution/Pages/Inventory/IncomingGoods/Index.cshtml.cs
@@ -29,10 +29,12 @@ namespace SumerBusinessSolution.Pages.Inventory.IncomingGoods
         public Warehouse Warehouse { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "من")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime SearchFromDate { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "الى")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime SearchToDate { get; set; }
 
