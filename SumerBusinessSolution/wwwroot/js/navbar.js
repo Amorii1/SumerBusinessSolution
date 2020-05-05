313185847b62b81d6e8937eeca936c0f96c35338
Omar Osamah <Amoriimessi@users.noreply.github.com>
2020-05-05 05:57:16 +0300

    sidebar+somechanges

---
+++ SumerBusinessSolution/wwwroot/js/navbar.js
@@ -0,0 +1,7 @@
+﻿const hamburger = document.querySelector(".hamburger");
+const sideBar = document.querySelector(".wrapper .sidebar");
+const links = document.querySelectorAll(".wrapper .sidebar li");
+
+hamburger.addEventListener("click", () => {
+    sideBar.classList.toggle("open");
+});
